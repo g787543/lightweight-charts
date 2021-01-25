d35fc70ac7a9ce71ad3336106c79bdd7fd2f95b1
Hans <ck235qwer@gmail.com>
2021-01-25 18:23:50 +0800

    line-renderer add yClose and closeYCord and remove unuse code and revise walkLine usage

--- src/renderers/line-renderer.ts
+++ src/renderers/line-renderer.ts
@@ -1,7 +1,8 @@
 import { PricedValue } from '../model/price-scale';
+import { CustomPriceLineOptions } from '../model/series-options';
 import { SeriesItemsIndexesRange, TimedValue } from '../model/time-data';
 
-import { LinePoint, LineStyle, LineType, LineWidth, setLineStyle } from './draw-line';
+import { LinePoint, LineStyle, LineType, LineWidth } from './draw-line';
 import { ScaledRenderer } from './scaled-renderer';
 import { walkLine } from './walk-line';
 
@@ -9,7 +10,8 @@ export type LineItem = TimedValue & PricedValue & LinePoint;
 
 export interface PaneRendererLineData {
 	lineType: LineType;
-
+	yClose?: CustomPriceLineOptions;
+	closeYCord: number | null;
 	items: LineItem[];
 
 	barWidth: number;
@@ -34,22 +36,28 @@ export class PaneRendererLine extends ScaledRenderer {
 		}
 
 		ctx.lineCap = 'butt';
-		ctx.lineWidth = this._data.lineWidth;
-
-		setLineStyle(ctx, this._data.lineStyle);
-
-		ctx.strokeStyle = this._data.lineColor;
 		ctx.lineJoin = 'miter';
 
 		ctx.beginPath();
+
 		if (this._data.items.length === 1) {
 			const point = this._data.items[0];
 			ctx.moveTo(point.x - this._data.barWidth / 2, point.y);
 			ctx.lineTo(point.x + this._data.barWidth / 2, point.y);
 		} else {
-			walkLine(ctx, this._data.items, this._data.lineType, this._data.visibleRange);
+			walkLine(
+				ctx,
+				this._data.items,
+				{
+					lineType: this._data.lineType,
+					lineWidth: this._data.lineWidth,
+					lineStyle: this._data.lineStyle,
+					strokeStyle: this._data.lineColor,
+				},
+				this._data.visibleRange,
+				this._data.closeYCord,
+				this._data.yClose
+			);
 		}
-
-		ctx.stroke();
 	}
 }
